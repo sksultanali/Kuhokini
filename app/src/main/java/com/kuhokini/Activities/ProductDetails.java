1d642f5acf9947ca0c411f2aa663b65eec771fb3
sksultanali <isksultanali@gmail.com>
2025-05-31 17:14:02 +0530

    commit

--- app/src/main/java/com/kuhokini/Activities/ProductDetails.java
+++ app/src/main/java/com/kuhokini/Activities/ProductDetails.java
@@ -3,12 +3,15 @@ package com.kuhokini.Activities;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.kuhokini.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 
 public class ProductDetails extends AppCompatActivity {
 
@@ -16,6 +19,14 @@ public class ProductDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+//        getLifecycle().addObserver(binding.youtubePostView);
+//        binding.youtubePostView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+//            @Override
+//            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+//                youTubePlayer.loadVideo(videoModel.getYouTubeId(), 0f);
+//            }
+//        });
 
 
     }
